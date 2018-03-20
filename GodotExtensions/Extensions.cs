cb14517575436304be03c2b1231bae09b41556fa
Craig Dodge <craig@craigdodge.com>
2018-03-19 17:53:01 -0700

    Extension methods for working in Godot and creating a nuget to install into new Godot C# projects.

---
+++ GodotExtensions/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Godot;
+
+public static class GodotExtensions
+{ 
+    // Replacement for rand_range GDScript function
+    public static float FloatRange(this Random random, float min = 0.0f, float max = 1.0f)
+    {
+        return (float)(random.NextDouble() * (max - min) + min);
+    }
+
+    // Generic version of GetNode, with type conversion built in.
+    public static T GetNode<T>(this Node node, NodePath Name) where T : Node
+    {
+        T n = (T)node.GetNode(Name);
+        return n;
+    }
+}
